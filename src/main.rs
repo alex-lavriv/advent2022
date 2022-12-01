b919ed970fcd417c10604801c2c75be1b632f885
phantom-il-alex <alex@phantomauto.com>
2022-12-01 12:38:05 +0200

    second

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 use std::fs;
+
 fn main() {
-    first()
+    first();
+    first_b();
 }
 
 
-fn first(){
-    let contents = fs::read_to_string("/home/phantom-il-alex/development/advent2022/src/1.input").unwrap();
+fn first() {
+    let contents = fs::read_to_string("/home/phantom-il-alex/advent2022/src/1.input").unwrap();
     let contents = contents.split("\n\n");
-    let max = contents.into_iter().fold(0, |max, current|{
-        let sum:i32 = current.split("\n").into_iter().fold(0, |acc, x| acc + x.parse::<i32>().unwrap());
+    let max = contents.into_iter().fold(0, |max, current| {
+        let sum: i32 = current.split("\n").into_iter().fold(0, |acc, x| acc + x.parse::<i32>().unwrap());
         if sum > max {
             sum
-        }
-        else {
+        } else {
             max
         }
     });
     println!("The max is {max}");
+}
+fn first_b() {
+    let contents = fs::read_to_string("/home/phantom-il-alex/advent2022/src/1.input").unwrap();
+    let contents = contents.split("\n\n");
+
+    let mut sums:Vec<i32> = contents.into_iter().map(|current| {
+        current.lines().into_iter().fold(0, |acc, x| acc + x.parse::<i32>().unwrap())
+
+    }).collect();
+    sums.sort();
+    let sum = sums.as_slice()[sums.len()-3..].to_vec().iter().fold(0, |a, x| a + x);
+    println!("The max is {:?}", sum);
 }
