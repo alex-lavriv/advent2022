c8196942de5a31c4d0f45bdd61e40b8f6eda65ad
Alex Lavriv <alex@phantomauto.com>
2022-12-03 18:51:31 +0200

    again

--- day_3_again/src/main.rs
+++ day_3_again/src/main.rs
@@ -1,18 +1,6 @@
 fn main() {
     let input = include_str!("../3.input");
 
-
-    // let sum: u32 = input.lines().map(|line| {
-    //     let (p1, p2) = line.split_at(line.len() / 2);
-    //     if let Some(c) = p1.chars().find(|&c| p2.contains(c)) {
-    //         return if c.is_lowercase() {
-    //             c as u32 - 96
-    //         } else {
-    //             c as u32 - 38
-    //         };
-    //     } else { todo!() };
-    // }).sum();
-
     let sum: u32 = input.lines()
         .map(|line| line.split_at(line.len() / 2)).map(|(p1, p2)| p1.chars().find(|&c| p2.contains(c)).map(|c| {
         return if c.is_lowercase() {
