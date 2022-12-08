aeca30fd9bc402dda3942b22ec4c7409cccae9cc
phantom-il-alex <alex@phantomauto.com>
2022-12-08 14:25:12 +0200

    day 8

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -4,7 +4,10 @@ use std::fs::File;
 fn main() {
     let vec = parse_input();
     let ans = solve(&vec);
-    println!("The ans is {ans}")
+
+    println!("The ans is {ans}");
+    let max_view = get_score(&vec);
+    println!("The max_view is {max_view}");
 }
 
 fn solve(vec: &Vec<Vec<u8>>) -> usize {
@@ -20,6 +23,76 @@ fn solve(vec: &Vec<Vec<u8>>) -> usize {
     ans
 }
 
+fn get_score(vec: &Vec<Vec<u8>>) -> u32 {
+    let mut max = 0;
+    for i in 1..vec.len() - 1 {
+        for j in 1..vec[i].len() - 1 {
+            let m = is_score_single(vec, i, j);
+            if (max < m){
+                max = m;
+            }
+        }
+    }
+    max
+}
+
+fn is_score_single(vec: &Vec<Vec<u8>>, x: usize, y: usize) -> u32 {
+    let val = vec[x][y];
+    let mut score = 1;
+    let mut count = 0;
+    //left
+    for i in (0..=(y - 1)).rev() {
+        let entry = vec[x][i];
+        if entry >= val {
+
+            count += 1;
+            break;
+        } else {
+            count += 1;
+        }
+    }
+    score *= count;
+    count = 0;
+    //right
+    for i in y + 1..vec.len() {
+        let entry = vec[x][i];
+        if entry >= val {
+            count += 1;
+            break;
+        } else {
+            count += 1;
+        }
+    }
+    score *= count;
+    count = 0;
+    //top
+    for i in (0..=(x - 1)).rev() {
+        let entry = vec[i][y];
+        if entry >= val {
+            count += 1;
+            break;
+        } else {
+            count += 1;
+        }
+    }
+    score *= count;
+    count = 0;
+    //bottom
+    for i in (x + 1)..vec[x].len() {
+        let entry = vec[i][y];
+        if entry >= val {
+            count += 1;
+            break;
+        } else {
+            count += 1;
+        }
+    }
+    score *= count;
+
+
+    score
+}
+
 fn is_visible(vec: &Vec<Vec<u8>>, x: usize, y: usize) -> bool {
     let val = vec[x][y];
     let mut v_visible = true;
@@ -28,17 +101,18 @@ fn is_visible(vec: &Vec<Vec<u8>>, x: usize, y: usize) -> bool {
         if vec[i][y] >= val && i != x {
             h_visible = false;
         }
-        if i == x{
+        if i == x {
+            if h_visible { return true; }
             h_visible = true;
         }
     }
-
 
     for i in 0..vec[x].len() {
         if vec[x][i] >= val && i != y {
             v_visible = false;
         }
-        if i == y{
+        if i == y {
+            if v_visible { return true; }
             v_visible = true;
         }
     }
@@ -61,11 +135,13 @@ fn parse_input() -> Vec<Vec<u8>> {
 fn test()
 {
     let input = parse_input();
-    for vec in &input {
-        for c in vec {
-            print!("{c} ")
+    for i in 0..input.len() {
+        for j in 0..input[i].len() {
+            print!(" ({},{}) {} ",i,j ,input[i][j])
         }
         println!();
     }
-    assert_eq!(is_visible(&input, 3, 3), false);
+    //assert_eq!(is_visible(&input, 3, 3), false);
+    assert_eq!(is_score_single(&input, 1, 2), 4);
+    assert_eq!(is_score_single(&input, 3, 2), 8);
 }
